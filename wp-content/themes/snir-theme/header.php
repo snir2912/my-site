e7ca5e468c417509f275645824528940edacd867
snir2912 <you@example.com>
2025-09-19 16:26:42 +0300

    דשגג

--- wp-content/themes/snir-theme/header.php
+++ wp-content/themes/snir-theme/header.php
@@ -21,6 +21,28 @@
     ) );
     ?>
 </nav>
+            <div class="search-form-container">
+                <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+                    <label>
+                        <span class="screen-reader-text">חפש:</span>
+                        <input type="search" class="search-field" placeholder="חפש באתר..." value="<?php echo get_search_query(); ?>" name="s" autocomplete="off" />
+                    </label>
+                    <button type="submit" class="search-submit">
+                        <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                        </svg>
+                    </button>
+                </form>
+
+                <div id="search-results-live" class="search-results-live-panel">
+                    <ul class="results-list">
+                    </ul>
+                    <div class="all-results-link-container" style="display: none;">
+                        <a href="#" class="all-results-link">הצג את כל התוצאות</a>
+                    </div>
+                    <p class="no-results" style="display: none;">לא נמצאו תוצאות.</p>
+                </div>
+            </div>
             <div class="header-controls">
                 <button id="theme-toggle" class="theme-toggle">
                     <span class="icon-dark">🌙</span>
